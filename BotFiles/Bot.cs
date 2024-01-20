a20730695800ca7a8d2112384d6e9595e4a5c633
Hrvoje Culek <hrvoje.culek@gmail.com>
2024-01-21 00:31:00 +0100

    configJson

--- BotFiles/Bot.cs
+++ BotFiles/Bot.cs
@@ -13,7 +13,7 @@ namespace Botko.BotFiles
 
         public async Task RunAsync() 
         {
-            using (var fs = File.OpenRead("config.json"))
+            using (var fs = File.OpenRead(Environment.CurrentDirectory + "\\BotFiles\\config\\config.json"))
             {
                 using (var sr = new StreamReader(fs))
                 {
@@ -37,27 +37,83 @@ namespace Botko.BotFiles
             await Task.Delay(-1);
         }
 
+
+        //Bot slash commands builder
         private async Task Client_Ready()
         {
-            var globalCommand = new SlashCommandBuilder();
-            globalCommand.WithName("test");
-            globalCommand.WithDescription("test command");
+            List<ApplicationCommandProperties> applicationCommandProperties = new();
 
             try
             {
+                var testCommand = new SlashCommandBuilder();
+                testCommand.WithName(Commands.test);
+                testCommand.WithDescription("test command");
+                applicationCommandProperties.Add(testCommand.Build());
 
-                await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+                var quoteAddCommand = new SlashCommandBuilder();
+                quoteAddCommand.WithName(Commands.quoteAdd);
+                quoteAddCommand.WithDescription("example: 'quote add lorem ipsum dolor sit amet' to add quote");
+                quoteAddCommand.AddOption("quote", ApplicationCommandOptionType.String, "example: 'quote add lorem ipsum dolor sit amet' to add quote", true, false, false, null, null, null, null, null, null, 1, null);
+                applicationCommandProperties.Add(quoteAddCommand.Build());
+
+                var quoteIdCommand = new SlashCommandBuilder();
+                quoteIdCommand.WithName(Commands.quoteId);
+                quoteIdCommand.WithDescription("example: 'quote-id 4' to find quote with id 4");
+                quoteIdCommand.AddOption("id", ApplicationCommandOptionType.Integer, "example: 'quote-id 4' to find quote with id 4", true, false, false, null, null, null, null, null, null, 1, null);
+                applicationCommandProperties.Add(quoteIdCommand.Build());
+
+                var quoteFindCommand = new SlashCommandBuilder();
+                quoteFindCommand.WithName(Commands.quoteFind);
+                quoteFindCommand.WithDescription("example: 'quote find lorem' to find first quote containing phrase");
+                quoteFindCommand.AddOption("keywords", ApplicationCommandOptionType.String, "example: 'quote find lorem' to find first quote containing phrase", true, false, false, null, null, null, null, null, null, 4, null);
+                applicationCommandProperties.Add(quoteFindCommand.Build());
+
+                var quoteDelCommand = new SlashCommandBuilder();
+                quoteDelCommand.WithName(Commands.quoteDel);
+                quoteDelCommand.WithDescription("example: 'quote-del 4' to delete quote with id 4");
+                quoteDelCommand.AddOption("id", ApplicationCommandOptionType.Integer, "example: 'quote-del 4' to delete quote with id 4", true, false, false, null, null, null, null, null, null, 1, null);
+                applicationCommandProperties.Add(quoteDelCommand.Build());
+
+                var quoteRandCommand = new SlashCommandBuilder();
+                quoteRandCommand.WithName(Commands.quoteRand);
+                quoteRandCommand.WithDescription("posts one random quote");
+                applicationCommandProperties.Add(quoteRandCommand.Build());
+
+                //TODO implement
+                //var quoteBullshitStartCommand = new SlashCommandBuilder();
+                //quoteBullshitStartCommand.WithName("quote-bullshit-start");
+                //quoteBullshitStartCommand.WithDescription("starts posting random quotes");
+
+                //var quoteBullshitStopCommandCommand = new SlashCommandBuilder();
+                //quoteBullshitStopCommandCommand.WithName("quote-bullshit-stop");
+                //quoteBullshitStopCommandCommand.WithDescription("stops posting random quotes");
+
+                await _client.BulkOverwriteGlobalApplicationCommandsAsync(applicationCommandProperties.ToArray());
             }
             catch (ApplicationCommandException exception)
             {
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+                Console.WriteLine(json);
                 //TODO error loging
             }
 
         }
 
+
+        // Bot slash commands handler
         private async Task SlashCommandHandler(SocketSlashCommand command)
         {
-            await command.RespondAsync($"You executed {command.Data.Name}");
+            switch (command.Data.Name) 
+            {
+                case Commands.test:
+                    await command.RespondAsync($"You executed {command.Data.Name} command");
+                    break;
+                case Commands.quoteId:
+                    await command.RespondAsync($"You requested quote id {command.Data.Options.First().Value.ToString()} command");
+                    break;
+
+            }
+
         }
 
         private Task Log(LogMessage msg)
---
+++ BotFiles/config/Commands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botko.BotFiles.config
+{
+    public static class Commands
+    {
+        public const string test = "testis";
+        public const string quoteAdd = "quote-add";
+        public const string quoteId = "quote-id";
+        public const string quoteFind = "quote-find";
+        public const string quoteDel = "quote-del";
+        public const string quoteRand = "quote-rand";
+    }
+}
