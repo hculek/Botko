312235f9c6521992c209606f837ab9cdc3efb897
Hrvoje Culek <hrvoje.culek@gmail.com>
2024-01-21 00:38:10 +0100

    Cleanup.

--- BotFiles/config/Commands.cs
+++ BotFiles/config/Commands.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Botko.BotFiles.config
+﻿namespace Botko.BotFiles.config
 {
     public static class Commands
     {
-        public const string test = "testis";
+        public const string test = "test";
         public const string quoteAdd = "quote-add";
         public const string quoteId = "quote-id";
         public const string quoteFind = "quote-find";
--- BotFiles/config/ConfigJSON.cs
+++ BotFiles/config/ConfigJSON.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 
 namespace Botko.BotFiles.config
 {
